639f28769dbf77beccbc0d2b92f9d31bbe1c82e3
awesomemin <saogig0503@naver.com>
2024-11-29 00:33:02 +0900

    로그인 페이지 작성

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,11 +4,7 @@ export default function SignUp() {
   return (
     <>
       <div className="flex flex-col h-screen">
-        <h1 className="mt-[60px] text-2xl font-semibold">
-          ID와 비밀번호 입력만으로
-          <br />
-          10초만에 시작해요
-        </h1>
+        <h1 className="mt-[60px] text-2xl font-semibold text-center">로그인</h1>
         <form className="mt-[55px]">
           <label className="text-sm font-semibold text-gray ml-2">ID</label>
           <div className="flex items-center w-full h-[60px] border border-lightGray rounded-2xl bg-background mb-6 mt-2">
@@ -18,21 +14,20 @@ export default function SignUp() {
           <label className="text-sm font-semibold text-gray ml-2">
             비밀번호
           </label>
-          <div className="flex items-center w-full h-[60px] border border-lightGray rounded-2xl bg-background mb-6 mt-2">
+          <div className="flex items-center w-full h-[60px] border border-lightGray rounded-2xl bg-background mt-2">
             <MdLock className="ml-[18px] text-lightGray w-6 h-6" />
             <input className="w-full mx-3 bg-background outline-none"></input>
           </div>
-          <label className="text-sm font-semibold text-gray ml-2">닉네임</label>
-          <div className="flex items-center w-full h-[60px] border border-lightGray rounded-2xl bg-background mt-2">
-            <MdTagFaces className="ml-[18px] text-lightGray w-6 h-6" />
-            <input className="w-full mx-3 bg-background outline-none"></input>
-          </div>
+
           <button className=" w-full h-16 bg-primary rounded-2xl text-lg font-semibold mt-10">
-            회원가입
+            로그인
           </button>
         </form>
         <div className="flex-grow"></div>
-        <button className="text-gray text-sm mb-20">이미 회원인가요?</button>
+        <button className="text-gray text-sm mb-20">
+          회원이 아니신가요?{' '}
+          <span className="text-primary underline">회원가입</span>
+        </button>
       </div>
     </>
   );

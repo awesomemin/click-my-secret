4ed4b89769d5bbf2487ae3ed7760044fed81e194
awesomemin <saogig0503@naver.com>
2024-11-29 14:23:41 +0900

    스피너 컴포넌트 추가

---
+++ src/components/spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="h-8 w-8 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}

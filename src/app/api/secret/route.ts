519bab124dbb19ec7288d63e2f4ab3bf864f690d
awesomemin <saogig0503@naver.com>
2024-11-30 23:35:59 +0900

    비밀 정보 가져오기 api 구현

--- src/app/api/secret/route.ts
+++ src/app/api/secret/route.ts
@@ -9,6 +9,91 @@ type Body = {
   revealCount: number;
 };
 
+export async function GET(request: NextRequest) {
+  const token = request.cookies.get('jwtToken');
+  const secretId = request.nextUrl.searchParams.get('secretId');
+  let userInfo;
+  if (token) {
+    userInfo = await verifyJWTToken(token.value);
+  }
+
+  //secretId가 오지 않았을 때
+  if (!secretId) {
+    return NextResponse.json(
+      { error: '비밀 ID가 제공되지 않았습니다.' },
+      { status: 400 }
+    );
+  }
+
+  //자신의 비밀에 대한 요청일 때
+  if (userInfo && (await hashSecretId(userInfo.loginId)) === secretId) {
+    try {
+      const secretInfo = await prisma.secret.findUnique({
+        where: {
+          id: secretId,
+        },
+        select: {
+          hint: true,
+          content: true,
+          revealCount: true,
+        },
+      });
+
+      if (!secretInfo)
+        throw new Error('나의 비밀 정보를 가져오는 중 에러가 발생했습니다.');
+
+      return NextResponse.json({
+        hint: secretInfo.hint,
+        content: secretInfo.content,
+        revealCount: secretInfo.revealCount,
+        ownerNickname: userInfo.nickname,
+      });
+    } catch (error) {
+      console.error('나의 비밀 정보를 가져오는 중 에러가 발생했습니다.', error);
+      return NextResponse.json(
+        { error: '나의 비밀 정보를 가져오는 중 에러가 발생했습니다.' },
+        { status: 500 }
+      );
+    }
+  }
+
+  //로그인 하지 않았거나, 타인의 비밀에 대한 요청일 때
+  try {
+    const secretInfo = await prisma.secret.findUnique({
+      where: {
+        id: secretId,
+      },
+      select: {
+        hint: true,
+        //content: false,
+        revealCount: true,
+        owner: {
+          select: {
+            nickname: true,
+          },
+        },
+      },
+    });
+
+    if (!secretInfo)
+      throw new Error('비밀 정보를 가져오는 중 에러가 발생했습니다.');
+
+    return NextResponse.json({
+      hint: secretInfo.hint,
+      revealCount: secretInfo.revealCount,
+      ownerNickname: secretInfo.owner.nickname,
+    });
+  } catch (error) {
+    console.error('비밀 정보를 가져오는 중 에러가 발생했습니다.', error);
+    return NextResponse.json(
+      {
+        error: '비밀 정보를 가져오는 중 에러가 발생했습니다.',
+      },
+      { status: 500 }
+    );
+  }
+}
+
 export async function POST(request: NextRequest) {
   const token = request.cookies.get('jwtToken');
   let userInfo: CustomJwtPayLoad | undefined;

2999524f43fa2133c5b75351ec9808be708a2d03
awesomemin <saogig0503@naver.com>
2024-12-02 16:58:28 +0900

    비밀 페이지 정보 불러오는 기능 구현

--- src/app/secret/[secretId]/page.tsx
+++ src/app/secret/[secretId]/page.tsx
@@ -3,18 +3,45 @@ import Countdown from '@/components/countdown';
 import Key from '@/../public/key.png';
 import Image from 'next/image';
 import Leaderboard from './leaderboard';
+import { cookies, headers } from 'next/headers';
 
 const gamja = Gamja_Flower({
   subsets: ['latin'],
   weight: '400',
 });
 
-export default function SecretPage() {
+export default async function SecretPage({
+  params,
+}: {
+  params: Promise<{ secretId: string }>;
+}) {
+  const secretId = (await params).secretId;
+  const headersList = await headers();
+  const cookieStore = await cookies();
+  const jwtToken = cookieStore.get('jwtToken');
+  const host = headersList.get('host');
+  const protocol = headersList.get('x-forwarded-proto');
+
+  const response = await fetch(
+    `${protocol}://${host}/api/secret?secretId=${secretId}`,
+    {
+      headers: {
+        Cookie: `jwtToken=${jwtToken?.value}`,
+      },
+    }
+  );
+  const data = await response.json();
+  const nickname: string = data.ownerNickname;
+  const hint: string = data.hint;
+  const content: string | undefined = data.content;
+  const contentLength: number | undefined = data.contentLenght;
+  const revealCount: number = data.revealCount;
+
   return (
     <div className="flex flex-col h-screen">
       <div className={`flex flex-col`}>
         <h1 className="text-2xl font-semibold text-center mt-[60px] mb-8">
-          <span className="text-primary">{'어썸민'}</span>님의 비밀
+          <span className="text-primary">{nickname}</span>님의 비밀
         </h1>
         <Countdown />
         <div className="w-full mt-7">
@@ -24,23 +51,25 @@ export default function SecretPage() {
           <div
             className={`flex items-center justify-center w-full h-[60px] border border-lightGray rounded-2xl bg-inputBg mt-2 text-sm ${gamja.className}`}
           >
-            {'hint'}
+            {hint}
           </div>
         </div>
         <div className="w-full mt-4">
           <label className="text-sm font-semibold text-gray ml-2">
-            비밀 내용
+            비밀 내용{content && ' (나만 볼 수 있어요)'}
           </label>
           <div
             className={`w-full break-words p-3 overflow-y-scroll border border-lightGray rounded-2xl bg-inputBg mt-2 text-sm ${gamja.className}`}
           >
-            <div className="blur-sm">비밀은 정당한 방법으로 확인하세요 ^^</div>
+            <div className={`${!content && 'blur-sm'}`}>
+              {content ? content : '비밀'}
+            </div>
           </div>
         </div>
         <p className="text-gray text-sm text-center mt-4">
           2024. 12. 24. 22:00 기준
           <br />
-          클릭 수 상위 {2}명에게만
+          클릭 수 상위 {revealCount}명에게만
           <br />
           비밀이 공유돼요.
         </p>

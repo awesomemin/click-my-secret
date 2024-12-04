da9df0a239f29b93a4da4ec9ab2999d23d3da245
awesomemin <saogig0503@naver.com>
2024-12-04 19:02:03 +0900

    비밀이 이미 있는 유저가 '비밀 만들러 가기'를 눌렀을 때 본인의 비밀 페이지로 리다이렉트

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { CustomJwtPayLoad, verifyJWTToken } from './app/lib/auth';
+import { hashSecretId } from './app/lib/secret';
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('jwtToken');
@@ -25,7 +26,9 @@ export async function middleware(request: NextRequest) {
 
     if (hasSecret) {
       // 비밀이 이미 있을 때
-      return NextResponse.redirect(new URL('/', request.url)); //본인의 비밀 페이지로 이동시켜야 함 (추후 구현)
+      return NextResponse.redirect(
+        new URL(`/secret/${await hashSecretId(userInfo.loginId)}`, request.url)
+      ); //본인의 비밀 페이지로 이동시켜야 함 (추후 구현)
     }
 
     // 비밀이 없을 때는 정상 진행

75eb10abd9b8309fde2c32e1b26c85566b30b58b
awesomemin <saogig0503@naver.com>
2024-12-05 10:46:23 +0900

    자신의 비밀을 클릭했을 때 예외 api측 구현

--- src/app/api/click/route.ts
+++ src/app/api/click/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import redis, { loadDataToRedis } from '@/lib/redis';
 import { verifyJWTToken } from '@/lib/auth';
+import { hashSecretId } from '@/lib/secret';
 
 export async function POST(request: NextRequest) {
   const token = request.cookies.get('jwtToken');
@@ -21,6 +22,12 @@ export async function POST(request: NextRequest) {
   }
 
   const secretId = request.nextUrl.searchParams.get('secretId');
+  if ((await hashSecretId(userInfo.loginId)) === secretId) {
+    return NextResponse.json(
+      { error: '자신의 비밀은 클릭할 수 없습니다.' },
+      { status: 400 }
+    );
+  }
   const key = `user:${userInfo.userId}:${secretId}:clicks`;
   await redis.incr(key);
   return NextResponse.json({ message: '클 릭' });

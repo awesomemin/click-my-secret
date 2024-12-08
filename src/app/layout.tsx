79b77e659abd5e29d47dd09f3f0558612198da7c
awesomemin <saogig0503@naver.com>
2024-12-08 17:23:45 +0900

    metadata description 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,8 @@ const pretendard = localFont({
 
 export const metadata: Metadata = {
   title: '내 비밀을 눌러봐',
-  description: 'click my secret',
+  description:
+    '이번 크리스마스, 가장 많이 클릭한 친구에게만 내 비밀을 공유해보세요.',
 };
 
 export default function RootLayout({

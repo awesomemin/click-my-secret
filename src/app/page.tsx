4b9239254453db98c876cf7fb1a2d62984b116c2
awesomemin <saogig0503@naver.com>
2024-12-08 15:36:29 +0900

    홈화면에 카운트다운 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,23 @@ import Lock from '../../public/lock.png';
 import Link from 'next/link';
 import KakaoAdfit from '@/components/kakaoAdfit';
 import SideMenu from '@/components/sideMenu';
+import Countdown from '@/components/countdown';
 
 export default function Home() {
   return (
     <>
       <SideMenu />
       <div className="flex flex-col items-center h-screen">
-        <Image src={Lock} alt="lock image" className="mt-48" />
+        <Image src={Lock} alt="lock image" className="mt-32" />
         <p className="font-semibold text-xl mt-2  ">이번 크리스마스,</p>
         <p className="font-semibold text-2xl text-center">
           가장 많이 클릭한 친구에게만
           <br />
           나의 비밀을 공유해보세요
         </p>
-        <div className="flex-grow"></div>
+        <div className="flex-grow flex flex-col items-center justify-center">
+          <Countdown />
+        </div>
         <Link
           href="/secret/make"
           className="w-full h-16 bg-primary rounded-2xl text-lg font-semibold flex items-center justify-center mb-40"
